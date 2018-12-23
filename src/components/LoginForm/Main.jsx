3cbf8f468ad0a9396e4cfe11f19b3f6d22823b0e
davidfunk13 <davefunk135@gmail.com>
2018-12-23 03:34:08 -0800

    as

--- src/components/LoginForm/Main.jsx
+++ src/components/LoginForm/Main.jsx
@@ -4,10 +4,14 @@ import Reddit from "../../utils/Reddit";
 
 let authorize = `https://www.reddit.com/api/v1/authorize?client_id=${process.env.REACT_APP_CLIENT_ID}&response_type=${process.env.REACT_APP_RESPONSE_TYPE}&state=${process.env.REACT_APP_RANDOM_STRING}&redirect_uri=${process.env.REACT_APP_REDIRECT_URI}&scope=${process.env.REACT_APP_SCOPE_STRING}`
 export default class Main extends Component {
+    state = {
+        token: ''
+    }
     componentDidMount() {
         let token = Reddit.authorize()
-        if(token !== undefined){
-            console.log(token)
+        if (token !== undefined) {
+            this.setState({ token: token })
+            console.log(this.state.token);
         }
     }
     render() {
