4e9d6b3c9b854b5a81d1e4b34b84f947391ad303
davidfunk13 <davefunk135@gmail.com>
2019-01-09 16:38:08 -0800

    now fetching and returning saved posts

--- src/components/Saved/Saved.jsx
+++ src/components/Saved/Saved.jsx
@@ -2,19 +2,26 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom'
 import { Reddit } from '../../utils/Reddit';
 export default class Saved extends Component {
+    state = {
+        saved: []
+    }
     componentDidMount() {
-        if (this.props.user) { 
-            Reddit.fetchSaved(this.props.user.data.name, sessionStorage.getItem('t')).then(response => { 
-                this.setState({saved: response.data})
-
-               })        }
+        if (this.props.user) {
+            Reddit.fetchSaved(this.props.user.data.name, sessionStorage.getItem('t')).then(response => {
+                this.setState({ saved: response.data.data.children })
+            });
+        }
 
     }
     render() {
         return (
             <div>
                 <p>Saved</p>
-                <button onClick={() => { console.log(this.state) }}></button>
+                <div>
+                    {this.state.saved.length ? this.state.saved.map(saved => {
+                        return <div key={saved.data.id}>{saved.data.title}</div>
+                    }) : <div>Loading...</div>}
+                </div>
                 <Link to="/">Back</Link>
             </div>
         )
