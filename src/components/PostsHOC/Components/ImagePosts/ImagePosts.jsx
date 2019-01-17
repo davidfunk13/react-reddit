f613d603b18a4a926129bbda4cec3bb25809f562
davidfunk13 <davefunk135@gmail.com>
2019-01-16 20:29:05 -0800

    working on grid

--- src/components/PostsHOC/Components/ImagePosts/ImagePosts.jsx
+++ src/components/PostsHOC/Components/ImagePosts/ImagePosts.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import { GridList, GridListTile, Modal } from '@material-ui/core';
 import thumb from '../AllPosts/thumb.png'
+let list = [];
 
 export default class ImageGridList extends Component {
   state = {
@@ -8,11 +9,13 @@ export default class ImageGridList extends Component {
     current: {
       img: '',
       title: ''
-    }
+    },
+    tiles: [],
   }
   componentDidMount() {
 
   }
+
   handleOpen = (img, title) => {
     this.setState({
       isOpen: true,
@@ -26,34 +29,61 @@ export default class ImageGridList extends Component {
     this.setState({ isOpen: false });
   }
 
+  imgError = (image) => {
+    image.onerror = "";
+    image.src = "../AllPosts/thumb.png";
+    return true;
+  }
+
   render() {
     const { classes } = this.props.store;
     const posts = this.props.posts.filter(post => post.kind === 't3' && (post.data.url.includes('gif') || post.data.url.includes('jpg') || post.data.url.includes('jpeg') || post.data.url.includes('png')))
-    let list = [];
-    posts.map(post => {
-      let tile = {
-        img: post.data.url,
-        cols: Math.floor((Math.random() * 2) + 1)
+    let prevCols = null;
+    let tile;
+    posts.map((post, index) => {
+      if (index === 0) {
+        tile = {
+          img: post.data.url,
+          title: '',
+          cols: Math.floor((Math.random() * 3) + 1)
+        }
+        prevCols = tile.cols;
+        list.push(tile);
+        return;
+        console.log(`prevCols now set to ${prevCols}`)
       }
-      list.push(tile);
+      if(prevCols !== null){
+        console.log(prevCols)
+        if(3-prevCols !== 0){
+          tile = {
+            img: post.data.url,
+            title: '',
+            cols: 3 - prevCols
+          };
+          prevCols = tile.cols;
+          list.push(tile);
+          return;
+        }
+        if (3-prevCols === 0){
+          tile = {
+            img: post.data.url,
+            title: '',
+            cols:  Math.floor((Math.random() * 3) + 1)
+          }
+          prevCols = tile.cols;
+          list.push(tile);
+          return;
+        }
+      }
     });
     return (
       <div>
         <GridList className={classes.gridList} cols={3}>
-          {list.map((tile, index) => { 
-            function ensureGrid() {
-              if (index !== 0){
-                console.log('hit')
-                console.log(list[index-1])
-                // if (tile[index - 1].cols === 2){
-                //   console.log('previos tile was 2')
-                // }
-              }
-            }
-            ensureGrid()
-              return <GridListTile key={tile.img} cols={tile.cols}>
-                <img src={tile.img || thumb} alt=""/>
-               </GridListTile>
+          {list.map((tile, index) => {
+            console.log(tile)
+            return <GridListTile key={tile.img} cols={tile.cols}>
+              <img onError={(e) => { e.target.onerror = null; e.target.src = thumb }} src={tile.img} alt="" />
+            </GridListTile>
           })}
         </GridList>
       </div>
