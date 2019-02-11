4d093cf7f59a60ebee2efb0a1ebd5739a19741c4
Dave <davefunk135@gmail.com>
2019-02-10 22:40:41 -0800

    finally might have caught all the posts without any edge cases

--- src/utils/Reddit.js
+++ src/utils/Reddit.js
@@ -17,7 +17,8 @@ export const Reddit = {
             });
     },
     sortPosts: (post, props) => {
-        let wasPostCaught = false;
+        let extensions = ['gifv', 'gif', 'jpg', 'jpeg', 'png'];
+        let fileExtension;
         let post1 = {
             domain: post.data.domain,
             url: post.data.url,
@@ -25,53 +26,27 @@ export const Reddit = {
             body: post.data.body
         };
         if (post1.body) {
-           wasPostCaught = !wasPostCaught
-            return <Comment post={post} {...props} />
+            return <Comment key={post.data.id} post={post} {...props} />
         }
         if (post1.domain.includes('gfycat')) {
-            wasPostCaught = !wasPostCaught
-            return <Video post={post} {...props} />;
+            return <Video key={post.data.id} post={post} {...props} />;
         }
         if (post1.domain.includes('imgur')) {
             if (post1.url.includes('gifv')) {
-                wasPostCaught = !wasPostCaught
-                return <Video post={post} {...props} />;
+                return <Video key={post.data.id} post={post} {...props} />;
             } else {
-                wasPostCaught = !wasPostCaught
-                return <Image post={post} {...props} />
+                return <Image key={post.data.id} post={post} {...props} />
             }
         }
-        return wasPostCaught ? null : <Link post={post} {...props}/>
-        // console.log(post.data);
-        // let extensions = ['gifv', 'gif', 'jpg', 'jpeg', 'png'];
-        // let fileExtension;
-        // if (post.data.domain && post.data.domain.includes('gfycat')) {
-        //     return <Video key={post.data.id} post={post} {...props} />
-        // }
-        // //checks if domain is imgur, then checks if its a video. else returns as image.
-        // if (post.data.domain && post.data.domain.includes('imgur')) {
-        //     if (post.data.url.includes('gifv')) {
-        //         return <Video key={post.data.id} post={post} {...props} />
-        //     } else {
-        //         return <Image key={post.data.id} post={post} {...props} />
-        //     }
-        // }
-        // // for all posts not under the imgur domain, if the url key is present, check the file extension. return if its an image.
-        // if (("url" in post.data)) {
-        //     fileExtension = post.data.url.lastIndexOf('.');
-        //     fileExtension = post.data.url.substr(fileExtension + 1);
-        //     return extensions.map(ext => {
-        //         if (ext === fileExtension) {
-        //             return <Image key={post.data.id} post={post} {...props} />
-        //         }
-        //     })
-
-        // }
-
-        // //comment post 1
-        // if (("body" in post.data)) {
-        //     console.log(post.data.body)
-        //     return <Comment key={post.data.id} post={post} {...props} />
-        // }
+        if (("url" in post.data)) {
+            fileExtension = post.data.url.lastIndexOf('.');
+            fileExtension = post.data.url.substr(fileExtension + 1);
+            return extensions.map(ext => {
+                if (ext === fileExtension) {
+                    return <Image key={post.data.id} post={post} {...props} />
+                }
+            })
+        }
+        console.log(post);
     }
 }
