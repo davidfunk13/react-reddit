6b63c7847f1691944b9764de14067b5d4e548189
davidfunk13 <davefunk135@gmail.com>
2019-02-12 17:06:49 -0800

    formatting error fixed in reddit.js

--- src/utils/Reddit.js
+++ src/utils/Reddit.js
@@ -1,30 +1,17 @@
 import axios from 'axios';
 import React from 'react';
-import {
-    Comment,
-    Link,
-    Image,
-    Video
-} from '../components/PostsHOC/Components/AllPosts/Components/Posts/index';
+import { Comment, Link, Image, Video } from '../components/PostsHOC/Components/AllPosts/Components/Posts/index';
 
 
 export const Reddit = {
     masterUser: (token) => {
-        return axios.get('https://oauth.reddit.com/api/v1/me', {
-                headers: {
-                    Authorization: 'Bearer ' + token
-                }
-            })
+        return axios.get('https://oauth.reddit.com/api/v1/me', { headers: { Authorization: 'Bearer ' + token } })
             .catch(err => {
                 return err.response
             });
     },
     fetchSaved: (user, token) => {
-        return axios.get(`https://oauth.reddit.com/user/${user}/saved?raw_json=1`, {
-                headers: {
-                    Authorization: 'Bearer ' + token
-                }
-            })
+        return axios.get(`https://oauth.reddit.com/user/${user}/saved?raw_json=1`, { headers: { Authorization: 'Bearer ' + token } })
             .catch(err => {
                 console.log(err.response);
                 return err.response
@@ -40,48 +27,16 @@ export const Reddit = {
             body: post.data.body
         };
         if (post1.body) {
-            return <Comment key = {
-                post.data.id
-            }
-            post = {
-                post
-            } {
-                ...props
-            }
-            />
+            return <Comment key={post.data.id} post={post} {...props} />
         }
         if (post1.domain.includes('gfycat')) {
-            return <Video key = {
-                post.data.id
-            }
-            post = {
-                post
-            } {
-                ...props
-            }
-            />;
+            return <Video key={post.data.id} post={post} {...props} />;
         }
         if (post1.domain.includes('imgur')) {
             if (post1.url.includes('gifv')) {
-                return <Video key = {
-                    post.data.id
-                }
-                post = {
-                    post
-                } {
-                    ...props
-                }
-                />;
+                return <Video key={post.data.id} post={post} {...props} />;
             } else {
-                return <Image key = {
-                    post.data.id
-                }
-                post = {
-                    post
-                } {
-                    ...props
-                }
-                />
+                return <Image key={post.data.id} post={post} {...props} />
             }
         }
         if (("url" in post.data)) {
@@ -96,25 +51,9 @@ export const Reddit = {
                 }
             })
             if (isImage === true) {
-                return <Image key = {
-                    post.data.id
-                }
-                post = {
-                    post
-                } {
-                    ...props
-                }
-                />
+                return <Image key={post.data.id} post={post} {...props} />
             } else {
-                return <Link key = {
-                    post.data.id
-                }
-                post = {
-                    post
-                } {
-                    ...props
-                }
-                />
+                return <Link key={post.data.id} post={post} {...props} />
             }
         }
         console.log(post);
@@ -192,4 +131,9 @@ export const Reddit = {
         }
         return media;
     },
+    htmlParse: (htmlString) => {
+        let className = htmlString.replace('class', 'className');
+        console.log(className)
+    }
+
 }
