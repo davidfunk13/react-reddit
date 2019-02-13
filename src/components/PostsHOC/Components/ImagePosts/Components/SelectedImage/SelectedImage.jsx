b90f2e9a928cbf1ef8b771d01640508b0429919f
davidfunk13 <davefunk135@gmail.com>
2019-02-13 03:01:55 -0800

    all but youtube work. reddit video still needs some tweaking for size inside modal

--- src/components/PostsHOC/Components/ImagePosts/Components/SelectedImage/SelectedImage.jsx
+++ src/components/PostsHOC/Components/ImagePosts/Components/SelectedImage/SelectedImage.jsx
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import ReactHtmlParser from 'react-html-parser';
 
 export default class SelectedImage extends Component {
 
@@ -7,30 +6,34 @@ export default class SelectedImage extends Component {
     componentDidMount() {
         console.log(this.props.imageData)
     }
+    getGfycatSrc = (imageData) => {
+        console.log(imageData);
+        let srcWithQuotes = imageData.img.html.match(/src\=([^\s]*)\s/)[1];
+        let src = srcWithQuotes.substring(1, srcWithQuotes.length - 1)
+        return src;
+    }
 
     render() {
         const { classes } = this.props.store;
         const { imageData } = this.props;
         const { type } = imageData;
-        // const { html } = imageData.img;
-        // let srcWithQuotes = imageData.html.match(/src\=([^\s]*)\s/)[1];
-        // let src = srcWithQuotes.substring(1, srcWithQuotes.length - 1)
+
         return (
             <div>
                 {type === 'gfycat' ?
-                    <div style={{position: 'relative', paddingBottom: 'calc(70.80% + 44px)'}}>
-                        {/* <iframe src={src} frameBorder='0' scrolling='no' width='100%' height='100%' style={{position:'absolute',top:0,left:0}} allowFullScreen></iframe> */}
+                    <div style={{ position: 'relative', paddingBottom: 'calc(70.80% + 44px)' }}>
+                        <iframe src={this.getGfycatSrc(imageData)} frameBorder='0' scrolling='no' width='100%' height='100%' style={{ position: 'absolute', top: 0, left: 0 }} allowFullScreen></iframe>
                     </div>
                     :
                     type === 'redditVideo' ?
-                    <div>
-          <video autoPlay loop>
-                        <source src={imageData.img.fallback_url} type="video/mp4"></source>
-                    </video>
-                        </div>
+                    <div style={{maxWidth: '100%'}}>
+                            <video  autoPlay loop>
+                                <source src={imageData.img.fallback_url} type="video/mp4"></source>
+                            </video>
+                    </div>
                         :
-                        type === 'image' ?
-                            <div>image</div>
+                        type === 'image' ?                            
+                                <img style={{maxWidth: '100%'}} src={imageData.img} alt={imageData.title}/>
                             :
                             type === 'gifv' ?
                                 <div>gifv</div>
