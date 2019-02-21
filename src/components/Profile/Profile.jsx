fab79c4a1c4b809aeee550c6b54c733c2e9d88a2
Dave <davefunk135@gmail.com>
2019-02-20 20:16:17 -0800

    fine tuning ui adding buttons where needed and cleaning code up along the way.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,7 +1,7 @@
 
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
-import { Typography } from '@material-ui/core';
+import { Typography, Grid, Button } from '@material-ui/core';
 import { Reddit } from '../../utils/Reddit';
 import thumb from '../PostsHOC/assets/img/thumb.png';
 
@@ -22,23 +22,32 @@ export default class Profile extends Component {
 
     render() {
         const { open, classes, classNames } = this.props.store;
-        const {data, isFetching} = this.state;
+        const { data, isFetching } = this.state;
         return (
             <div>
                 {isFetching ?
                     <main className={classNames(classes.content, { [classes.contentShift]: open })} >
-                        <Typography>
-                            Fetching...
-                        </Typography>
+                        <Grid container direction={'column'} alignItems={'center'}>
+                            <Grid item xs={12}>
+                                <Typography align={'center'}>
+                                    Fetching...
+                                </Typography>
+                            </Grid>
+                        </Grid>
                     </main>
                     :
-                    <main className={classNames(classes.content, { [classes.contentShift]: open })} >
-                        <img onError={(e) => { e.target.onerror = null; e.target.src = thumb }} src={data.data.icon_img} alt="" />
-                        <Typography>
-                            {data.data.name}
-                        </Typography>
-                        
-                        <Link to='/'>Back</Link>
+                    <main style={{ height: '75vh' }} className={classNames(classes.content, { [classes.contentShift]: open })} >
+                        <Grid container direction={'column'} alignItems={'center'}>
+                            <Grid item xs={12}>
+                                <img onError={(e) => { e.target.onerror = null; e.target.src = thumb }} src={data.data.icon_img} alt="" />
+                            </Grid>
+                            <Grid item xs={12}>
+                                <Typography>
+                                    {data.data.name}
+                                </Typography>
+                            </Grid>
+                        <Link to='/'><Button variant={'contained'} color={'secondary'}>Back</Button></Link>
+                        </Grid>
                     </main>
                 }
             </div>
--- src/components/Unauthorized/Unauthorized.jsx
+++ src/components/Unauthorized/Unauthorized.jsx
@@ -1,6 +1,6 @@
 
 import React, { Component } from "react";
-import { Typography, Divider } from '@material-ui/core/';
+import { Typography, Divider, Grid } from '@material-ui/core/';
 // import classNames from "classnames"
 
 
@@ -9,24 +9,23 @@ export default class Unauthorized extends Component {
     render() {
         const { open, classes, classNames } = this.props.store;
         return (
-            <div>
                 <main className={classNames(classes.content, { [classes.contentShift]: open })} >
-                    <Typography component={'h1'} variant="h5" gutterBottom>
-                        Welcome
-                    </Typography>
-                    <Typography component={'p'} gutterBottom>
-                        I am a junior javascript developer, and this is my React based Reddit client!
-                    </Typography>
-                    <Divider />
-                    <Typography component={'h1'} variant="h5"  gutterBottom>
-                        About this App
-                    </Typography>
-                    <Typography component={'p'} gutterBottom>
-                        This was my two-in-one attempt at learning oAuth as well as MaterialUI. I am a fullstack developer, but wanted to take this project to work on polishing my front end capabilities.
-                    </Typography>
+                    <Grid container direction={'column'} alignContent={'center'}>
+                        <Typography component={'h1'} variant="h5" gutterBottom>
+                            Welcome
+                        </Typography>
+                        <Typography component={'p'} gutterBottom>
+                            I am a junior javascript developer, and this is my React based Reddit client!
+                        </Typography>
+                        <Divider />
+                        <Typography component={'h1'} variant="h5" gutterBottom>
+                            About this App
+                        </Typography>
+                        <Typography component={'p'} gutterBottom>
+                            This was my two-in-one attempt at learning oAuth as well as MaterialUI. I am a fullstack developer, but wanted to take this project to work on polishing my front end capabilities.
+                        </Typography>
+                    </Grid>
                 </main>
-            </div>
-
         );
     }
 }
